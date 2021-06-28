1327bd30a507e65b16ed96d4dcfd3f7692cca1a9
henk717 <henk717@msn.com>
2021-06-28 22:35:15 +0200

    The Mamba Installer Update
    Big overhaul of the installer, partially based on the #53  commit from LexSong.
    
    The following is new :
    - Conda has been replaced with MicroMamba, allows the dependencies to automatically download the best version of Python and prevents all the issues with people failing to download conda.
    - The installer now has more options so you can choose not to delete the existing files, and it has new optional virtual K: drive support to bypass all the pathing issues people are having (Sorry Windows 7 users, its still not compatible even now).
    - Docker support for Linux systems has been added including ROCm support.
    - Environment files are now used to more easily keep everything on track, and to allow conda users to manually create environments across all operating systems (ROCm is an outlier in this because i have to use AMD's Pytorch docker for now it was to much hassle getting their Conda to use the environment file to add it to this commit).
    - Play.bat has been changed to allow the virtual drive support, everything should still be compatible with old installations as I kept all the paths intact.

--- play.bat
+++ play.bat
@@ -1,6 +1,18 @@
 @echo off
+cd %~dp0
 TITLE KoboldAI - Client
+SET /P M=<loader.settings
+IF %M%==1 GOTO drivemap
+IF %M%==2 GOTO subfolder
+
+:subfolder
 call miniconda3\condabin\activate
-cls
 python aiserver.py
+cmd /k
+
+:drivemap
+subst K: miniconda3 >nul
+call K:\python\condabin\activate
+python aiserver.py
+subst K: /D
 cmd /k
