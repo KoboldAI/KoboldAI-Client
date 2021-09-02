8fc5baa94be5ef017edd1fd4c94e86223582cd1c
henk717 <henk717@msn.com>
2021-09-02 17:33:58 +0200

    Cleanup

--- commandline.bat
+++ commandline.bat
@@ -6,7 +6,6 @@ IF %M%==1 GOTO drivemap
 IF %M%==2 GOTO subfolder
 
 :subfolder
-umamba.exe install --no-shortcuts -r miniconda3 -n base -c conda-forge cython pip
 call miniconda3\condabin\activate
 cmd /k
 
