2d1561aa55e76691527f4dda3c598155c1a951af
henk717 <henk717@msn.com>
2021-12-14 15:24:56 +0100

    Version Switcher
    Allows people to easily switch between different versions of KoboldAI. The stable one, United or their own. Compatible with my earlier update script.

---
+++ switch-versions.bat
@@ -0,0 +1,40 @@
+@echo off
+%~d0
+cd %~dp0
+TITLE KoboldAI - Branch Switcher
+SET /P M=<loader.settings
+IF %M%==1 GOTO drivemap
+IF %M%==2 GOTO subfolder
+
+:subfolder
+call miniconda3\condabin\activate
+GOTO GIT
+
+:drivemap
+subst K: miniconda3 >nul
+call K:\python\condabin\activate
+GOTO GIT
+
+:GIT
+ECHO 1. KoboldAI Main (The Official stable version of KoboldAI)
+ECHO 2. KoboldAI United (Development Version, new features but may break at any time)
+SET /P V=Enter your desired version or type your own GIT URL:
+IF %V%==1 (
+SET origin=https://github.com/koboldai/koboldai-client
+SET branch=main
+) ELSE (
+	IF %V%==2 (
+		SET origin=https://github.com/henk717/koboldai
+		SET branch=united
+	) ELSE (
+		SET origin=%v%
+		SET /P branch=Specify the GIT Branch:
+	)
+)
+
+git init     
+git remote remove origin
+git remote add origin %origin%    
+git fetch
+git checkout %branch% -f
+cmd /k
