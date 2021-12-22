9f86ca5be530b096ad3dafc59912d2621bccfac0
henk717 <henk717@msn.com>
2021-12-22 21:56:57 +0100

    Force Temp Location
    Conda breaks if the username has spaces when it tries to use temp, added a workaround that forces our directory to be used as temp for kobold.

--- commandline.bat
+++ commandline.bat
@@ -1,15 +1,19 @@
 @echo off
-cd %~dp0
+cd /D %~dp0
 TITLE CMD for KoboldAI Runtime
 SET /P M=<loader.settings
 IF %M%==1 GOTO drivemap
 IF %M%==2 GOTO subfolder
 
 :subfolder
+SET TEMP=%~DP0MINICONDA3
+SET TMP=%~DP0MINICONDA3
 call miniconda3\condabin\activate
 cmd /k
 
 :drivemap
 subst K: miniconda3 >nul
+SET TEMP=K:\
+SET TMP=K:\
 call K:\python\condabin\activate
 cmd /k
--- play.bat
+++ play.bat
@@ -1,18 +1,23 @@
 @echo off
-%~d0
-cd %~dp0
+cd /D %~dp0
 TITLE KoboldAI - Server
 SET /P M=<loader.settings
 IF %M%==1 GOTO drivemap
 IF %M%==2 GOTO subfolder
 
 :subfolder
+ECHO Runtime launching in subfolder mode
+SET TEMP=%~DP0MINICONDA3
+SET TMP=%~DP0MINICONDA3
 call miniconda3\condabin\activate
 python aiserver.py %*
 cmd /k
 
 :drivemap
+ECHO Runtime launching in K: drive mode
 subst K: miniconda3 >nul
+SET TEMP=K:\
+SET TMP=K:\
 call K:\python\condabin\activate
 python aiserver.py %*
 subst K: /D
