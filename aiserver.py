557d06238167ab6106872d8ba014a4d145808964
henk717 <henk717@msn.com>
2021-12-30 14:16:04 +0100

    Finetuneanon Models
    Uploaded with permission, so now Finetuneanon's models can be added to the main menu

--- aiserver.py
+++ aiserver.py
@@ -69,6 +69,8 @@ modellist = [
     ["Lit 6B (NSFW)", "hakurei/lit-6B", "12GB"],
     ["C1 6B (Chatbot)", "hakurei/c1-6B", "12GB"],
     ["Picard 2.7B (Novel)", "KoboldAI/GPT-Neo-2.7B-Picard", "6GB"],
+    ["Horni (NSFW)", "KoboldAI/GPT-Neo-2.7B-Horni", "6GB"],
+    ["Horni-LN (Novel/NSFW)", "KoboldAI/GPT-Neo-2.7B-Horni-LN", "6GB"],
     ["Shinen 2.7B (NSFW)", "KoboldAI/GPT-Neo-2.7B-Shinen", "6GB"],
     ["GPT-J 6B", "EleutherAI/gpt-j-6B", "12GB"],
     ["GPT-Neo 2.7B", "EleutherAI/gpt-neo-2.7B", "6GB"],
