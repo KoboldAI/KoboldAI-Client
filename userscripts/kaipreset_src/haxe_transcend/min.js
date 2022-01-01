b3ced30e378b4ce8ad9302f3956857852d299e37
Gnome Ann <>
2022-01-01 11:45:47 -0500

    Fix typo in Haxe test and add littleBigInt license

--- userscripts/kaipreset_src/haxe_transcend/min.js
+++ userscripts/kaipreset_src/haxe_transcend/min.js
@@ -6,7 +6,7 @@ const out_path = "out.min.lua";
 
 var data = fs.readFileSync(in_path, "utf8");
 data = luamin.minify(data);
-data = "-- Haxe trancendental test\n\
+data = '-- Haxe transcendental test\n\
 -- This is a script written in Haxe that prints the natural logarithm of the\n\
 -- golden ratio in base 10 to arbitrarily many digits.\n\
 \n\
@@ -24,5 +24,31 @@ data = "-- Haxe trancendental test\n\
 --\n\
 -- You should have received a copy of the GNU Affero General Public License\n\
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.\n\
-\n" + data + "\n";
+\n\
+--------------------------------------------------------------------------------\n\
+\n\
+-- License for littleBigInt:\n\
+\n\
+-- MIT License\n\
+--\n\
+-- Copyright (c) 2020 Sylvio Sell\n\
+--\n\
+-- Permission is hereby granted, free of charge, to any person obtaining a copy\n\
+-- of this software and associated documentation files (the "Software"), to deal\n\
+-- in the Software without restriction, including without limitation the rights\n\
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n\
+-- copies of the Software, and to permit persons to whom the Software is\n\
+-- furnished to do so, subject to the following conditions:\n\
+--\n\
+-- The above copyright notice and this permission notice shall be included in all\n\
+-- copies or substantial portions of the Software.\n\
+--\n\
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n\
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n\
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n\
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n\
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n\
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n\
+-- SOFTWARE.\n\
+\n' + data + "\n";
 fs.writeFileSync(out_path, data);
