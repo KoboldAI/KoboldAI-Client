c0bbe9f810c80b887f8d5b6fe17ae9524d463e31
ebolam <ebolam@gmail.com>
2022-02-06 19:04:13 -0500

    Reverted docker-cuda to mainline version.

--- docker-cuda/build_docker_images.sh
+++
@@ -1,5 +0,0 @@
-docker image build -f docker-cuda/Dockerfile_base . -t ebolam/koboldai_base:bare
-docker image build -f docker-cuda/Dockerfile_base_huggingface . -t ebolam/koboldai_base
-docker image build -f docker-cuda/Dockerfile_base_finetune . -t ebolam/koboldai_base:finetune
-docker image build -f docker-cuda/Dockerfile . -t ebolam/koboldai
-docker image build -f docker-cuda/Dockerfile_finetune . -t ebolam/koboldai:finetune
