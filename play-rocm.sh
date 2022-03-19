f1487a45513462a21c12dba498f04c7cac2190db
henk717 <henk717@msn.com>
2022-03-20 00:00:21 +0100

    New Linux Runtime

--- play-rocm.sh
+++ play-rocm.sh
@@ -1,4 +1,3 @@
-cd docker-rocm
-xhost +local:docker
-cp ../environments/rocm.yml env.yml
-docker-compose run --service-ports koboldai bash -c "cd /content && python3 aiserver.py $*"
+wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+bin/micromamba create -f environments/rocm.yml -r runtime -n koboldai-rocm -y
+bin/micromamba run -r runtime -n koboldai-rocm python aiserver.py
