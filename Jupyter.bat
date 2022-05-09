c5462ec48026781faf91c063a4bc221782272a24
Henk <henk@henk.tech>
2022-05-09 02:41:00 +0200

    Better Jupyter

--- Jupyter.bat
+++ Jupyter.bat
@@ -7,23 +7,23 @@ IF %M%==2 GOTO subfolder
 IF %M%==3 GOTO drivemap_B
 
 :subfolder
-umamba.exe install --no-shortcuts -r miniconda3 -n base -c conda-forge jupyter
+umamba.exe install --no-shortcuts -r miniconda3 -n base -c conda-forge jupyterlab
 call miniconda3\condabin\activate
-jupyter notebook
+jupyter-lab
 cmd /k
 
 :drivemap
 subst K: miniconda3 >nul
-umamba.exe install --no-shortcuts -r K:\python\ -n base -c conda-forge jupyter
+umamba.exe install --no-shortcuts -r K:\python\ -n base -c conda-forge jupyterlab
 call K:\python\condabin\activate
-jupyter notebook
+jupyter-lab
 subst K: /D
 cmd /k
 
 :drivemap_B
 subst B: miniconda3 >nul
-umamba.exe install --no-shortcuts -r B:\python\ -n base -c conda-forge jupyter
+umamba.exe install --no-shortcuts -r B:\python\ -n base -c conda-forge jupyterlab
 call B:\python\condabin\activate
-jupyter notebook
+jupyter-lab
 subst B: /D
 cmd /k
