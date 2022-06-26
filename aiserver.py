b99d1449c942286113df3c98ba7dd99137fcae64
vfbd <ve.forbryderne@gmail.com>
2022-06-26 13:15:55 -0400

    Remove trailing whitespace from submissions

--- aiserver.py
+++ aiserver.py
@@ -3495,6 +3495,7 @@ def actionsubmit(data, actionmode=0, force_submit=False, force_prompt_gen=False,
         if(not vars.gamestarted):
             vars.submission = data
             execute_inmod()
+            vars.submission = re.sub(r"[^\S\r\n]*([\r\n]*)$", r"\1", vars.submission)  # Remove trailing whitespace, excluding newlines
             data = vars.submission
             if(not force_submit and len(data.strip()) == 0):
                 assert False
@@ -3553,6 +3554,7 @@ def actionsubmit(data, actionmode=0, force_submit=False, force_prompt_gen=False,
                 data = applyinputformatting(data)
             vars.submission = data
             execute_inmod()
+            vars.submission = re.sub(r"[^\S\r\n]*([\r\n]*)$", r"\1", vars.submission)  # Remove trailing whitespace, excluding newlines
             data = vars.submission
             # Dont append submission if it's a blank/continue action
             if(data != ""):
