ea1d6e3e3dbfb4e52f4d8769aa5411d160b12a35
ebolam <ebolam@gmail.com>
2022-09-07 08:06:32 -0400

    Update to horde code for latest version

--- koboldai_settings.py
+++ koboldai_settings.py
@@ -805,12 +805,10 @@ class system_settings(settings):
                                 random.seed()
                                 if os.path.exists('./KoboldAI-Horde/venv/scripts/python.exe'):
                                     self._horde_pid = subprocess.Popen(['./KoboldAI-Horde/venv/scripts/python.exe', './KoboldAI-Horde/bridge.py', 
-                                                                            '--username', '{}'.format(random.randint(-1000000000000, 1000000000000)), '--password', '{}'.format(random.randint(-1000000000000, 1000000000000)), 
-                                                                            '--kai_name', '{}'.format(random.randint(-1000000000000, 1000000000000)), '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net"])
+                                                                            '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net"])
                                 else:
                                     self._horde_pid = subprocess.Popen(['./KoboldAI-Horde/venv/bin/python', './KoboldAI-Horde/bridge.py', 
-                                                                            '--username', '{}'.format(random.randint(-1000000000000, 1000000000000)), '--password', '{}'.format(random.randint(-1000000000000, 1000000000000)), 
-                                                                            '--kai_name', '{}'.format(random.randint(-1000000000000, 1000000000000)), '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net"])
+                                                                            '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net"])
                         else:
                             if self._horde_pid is not None:
                                 print("kill bridge")
