cd9e4175a9d49240f8b0a5b891c46af7266fa582
ebolam <ebolam@gmail.com>
2022-09-19 09:31:37 -0400

    Colab Fix

--- utils.py
+++ utils.py
@@ -223,7 +223,7 @@ def _download_with_aria2(aria2_config: str, total_length: int, directory: str = 
                 for k, v in lengths.items():
                     if k not in visited:
                         lengths[k] = (v[1], v[1])
-                koboldai_vars.downloaded_chunks += sum(v[0] for v in lengths.values())
+                koboldai_vars.downloaded_chunks = sum(v[0] for v in lengths.values())
                 bar.n = koboldai_vars.downloaded_chunks
                 bar.update()
                 time.sleep(0.1)
