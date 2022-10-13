4d712c1e21f2ef114381fc167fb460020683edad
ebolam <ebolam@gmail.com>
2022-10-13 13:47:22 -0400

    Added experimental features

--- koboldai_settings.py
+++ koboldai_settings.py
@@ -1097,7 +1097,7 @@ class system_settings(settings):
                                 random.seed()
                                 if os.path.exists('./KoboldAI-Horde/venv/scripts/python.exe'):
                                     self._horde_pid = subprocess.Popen(['./KoboldAI-Horde/venv/scripts/python.exe', './KoboldAI-Horde/bridge.py', 
-                                                                            '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net"])
+                                                                            '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net", "--log_file"])
                                 else:
                                     self._horde_pid = subprocess.Popen(['./KoboldAI-Horde/venv/bin/python', './KoboldAI-Horde/bridge.py', 
                                                                             '--kai_url', 'http://127.0.0.1:{}'.format(self.port), '--cluster_url', "http://koboldai.net"])
