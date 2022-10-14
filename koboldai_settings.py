b53dd86a7c0269c6d2830efe1aa2a30b627c80b4
somebody <onesome01@protonmail.com>
2022-10-14 17:35:13 -0500

    Fix for multiline action slice and dicer

--- koboldai_settings.py
+++ koboldai_settings.py
@@ -1548,7 +1548,7 @@ class KoboldStoryRegister(object):
         action_text = self.__str__()
         action_text = "{}{}{}".format("" if self.story_settings is None else self.story_settings.prompt, action_text, submitted_text)
         ###########action_text_split = [sentence, actions used in sentence, token length, included in AI context]################
-        action_text_split = [[x, [], 0, False] for x in re.findall(".*?[.!?]\s+", action_text)]
+        action_text_split = [[x, [], 0, False] for x in re.findall(".*?[.!?]\s+", action_text, re.S)]
         #The above line can trim out the last sentence if it's incomplete. Let's check for that and add it back in
         if len("".join([x[0] for x in action_text_split])) < len(action_text):
             action_text_split.append([action_text[len("".join([x[0] for x in action_text_split])):], [], 0, False])
