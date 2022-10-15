e278f9f04ffda98f495070e9e3a3f44d13fb47f7
ebolam <ebolam@gmail.com>
2022-10-15 19:25:52 -0400

    Remove Debug

--- static/koboldai.js
+++ static/koboldai.js
@@ -2975,7 +2975,6 @@ function update_context(data) {
 
 		let rgb = window.getComputedStyle(el)["background-color"].match(/(\d+), (\d+), (\d+)/).slice(1, 4).map(Number);
 
-		console.log(entry);
 		for (const [tokenId, token] of entry.tokens) {
 			let tokenColor = distortColor(rgb);
 			tokenColor = "#" + (tokenColor.map(dec2Hex2).join(""));
