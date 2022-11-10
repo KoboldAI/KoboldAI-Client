26bf04d865fabbb4859dd48b2a0b83ad73bb6b40
ebolam <ebolam@gmail.com>
2022-11-10 13:39:26 -0500

    New Docker process (will integrate with github to keep up to date)

---
+++ docker-standalone/docker-helper_new.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /opt/koboldai
+if [[ -n update ]];then
+	git pull
+fi
+
+#The goal here is to allow any directory in /content to be mapped to the appropriate dir in the koboldai dir
+if [[ ! -d "/content" ]];then
+	mkdir /content
+fi
+
+for FILE in *;do 
+	if [[ -d "/opt/koboldai/$FILE" ]];then
+		rm -rf /opt/koboldai/$FILE
+	fi
+	ln -s /content/$FILE /opt/koboldai/$FILE
+done
+
+
+#Previous parameters are now env vars in the docker container so they can be overwritten as desired
+PYTHONUNBUFFERED=1 ./play.sh
