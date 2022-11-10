dd73ce620f1a8aeff9d6eaf94c406b9ea046b4f6
ebolam <ebolam@gmail.com>
2022-11-10 16:08:31 -0500

    Docker update

--- docker-standalone/docker-helper_new.sh
+++ docker-standalone/docker-helper_new.sh
@@ -2,9 +2,6 @@
 cd /opt/koboldai
 if [[ -n update ]];then
 	git pull
-	cd KoboldAI-Horde/
-	git pull
-	cd ..
 fi
 
 #The goal here is to allow any directory in /content to be mapped to the appropriate dir in the koboldai dir
@@ -16,7 +13,8 @@ for FILE in *;do
 	if [[ -d "/opt/koboldai/$FILE" ]];then
 		rm -rf /opt/koboldai/$FILE
 	fi
-	ln -s /content/$FILE /opt/koboldai/$FILE
+	#ln -s /content/$FILE /opt/koboldai/$FILE
+	mount --bind /content/$FILE /opt/koboldai/$FILE
 done
 
 
