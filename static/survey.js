2500b23e4ac45306b3ae5e30e677d15d31e35e7f
somebody <onesome01@protonmail.com>
2022-12-14 18:31:04 -0600

    Update for model

--- static/survey.js
+++ static/survey.js
@@ -2,6 +2,7 @@ function $el(selector) { return document.querySelector(selector) }
 
 var socket = io.connect(window.location.origin, { transports: ['polling', 'websocket'], closeOnBeforeunload: false });
 var question = $el("#question").value;
+var model = $el("#model").value;
 
 function send_results() {
     var answer = "";
@@ -15,7 +16,7 @@ function send_results() {
     }
 
     console.log(answer);
-    socket.emit("answer", { "question": question, "answer": answer, "id": document.getElementById("id").value });
+    socket.emit("answer", { "question": question, "answer": answer, "id": document.getElementById("id").value, "model": model});
 }
 
 for (const child of $el("#choices-container").children) {
