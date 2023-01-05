9b9ff307e7d32e246c78334731daf880dda87c76
Henk <henk@henk.tech>
2023-01-05 19:57:46 +0100

    Docker changes
    The .github folder was causing workflow errors for everyone who didn't configure this correctly. I have updated the docker with the latest ideas for flexibility. If a quick build is desirable people can build upon the official KoboldAI United image a seperate base image should not be needed.

--- docker-standalone/docker-helper.sh
+++ docker-standalone/docker-helper.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 cd /opt/koboldai
 
-git pull --recurse-submodules && ./install_requirements.sh cuda
+if [[ -n update ]];then
+        git pull --recurse-submodules && ./install_requirements.sh cuda
+fi
 
 if [[ ! -v KOBOLDAI_DATADIR ]];then
 	mkdir /content
 	KOBOLDAI_DATADIR=/content
 fi
 
-mkdir $KOBOLDAI_DATADIR/stories
 if [[ ! -v KOBOLDAI_MODELDIR ]];then
 	mkdir $KOBOLDAI_MODELDIR/models
+	mkdir $KOBOLDAI_MODELDIR/functional_models
 fi
+
+mkdir $KOBOLDAI_DATADIR/stories
 mkdir $KOBOLDAI_DATADIR/settings
 mkdir $KOBOLDAI_DATADIR/softprompts
 mkdir $KOBOLDAI_DATADIR/userscripts
-#mkdir $KOBOLDAI_MODELDIR/cache
+mkdir $KOBOLDAI_DATADIR/presets
+mkdir $KOBOLDAI_DATADIR/themes
 
 cp -rn stories/* $KOBOLDAI_DATADIR/stories/
 cp -rn userscripts/* $KOBOLDAI_DATADIR/userscripts/
 cp -rn softprompts/* $KOBOLDAI_DATADIR/softprompts/
-
-rm stories
-rm -rf stories/
-rm userscripts
-rm -rf userscripts/
-rm softprompts
-rm -rf softprompts/
+cp -rn presets/* $KOBOLDAI_DATADIR/presets/
+cp -rn themes/* $KOBOLDAI_DATADIR/themes/
 
 if [[ ! -v KOBOLDAI_MODELDIR ]];then
 	rm models
 	rm -rf models/
-	#rm cache
-	#rm -rf cache/
 fi
 
-ln -s $KOBOLDAI_DATADIR/stories/ stories
-ln -s $KOBOLDAI_DATADIR/settings/ settings
-ln -s $KOBOLDAI_DATADIR/softprompts/ softprompts
-ln -s $KOBOLDAI_DATADIR/userscripts/ userscripts
 if [[ ! -v KOBOLDAI_MODELDIR ]];then
 	ln -s $KOBOLDAI_MODELDIR/models/ models
-	#ln -s $KOBOLDAI_MODELDIR/cache/ cache
+	ln -s $KOBOLDAI_MODELDIR/functional_models/ functional_models
 fi
+
+for FILE in $KOBOLDAI_DATADIR*
+do
+    FILENAME="$(basename $FILE)"
+	rm /opt/koboldai/$FILENAME
+	rm -rf /opt/koboldai/$FILENAME
+	ln -s $FILE /opt/koboldai/
+done
 
 PYTHONUNBUFFERED=1 ./play.sh --remote --quiet --override_delete --override_rename
