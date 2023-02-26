15f957260fac7b21dd86b31687fe725e975dfd33
somebody <onesome01@protonmail.com>
2023-02-26 13:35:54 -0600

    Model: Add rep pen for OAI API

--- model.py
+++ model.py
@@ -2549,6 +2549,7 @@ class OpenAIAPIInferenceModel(InferenceModel):
                 "max_tokens": max_new,
                 "temperature": gen_settings.temp,
                 "top_p": gen_settings.top_p,
+                "frequency_penalty": gen_settings.rep_pen,
                 "n": batch_count,
                 "stream": False,
             }
