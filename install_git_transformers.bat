90a7eb615351afd7901939c616b5165576198889
Henk <henk@henk.tech>
2023-03-17 12:40:08 +0100

    LLama tokenizer settings

---
+++ install_git_transformers.bat
@@ -0,0 +1,41 @@
+@echo off
+cd /D %~dp0
+SET CONDA_SHLVL=
+
+TITLE KoboldAI - Git Transformers Installer
+ECHO This script will replace the Transformers version with the latest Git Transformers which may contain breaking changes.
+ECHO If you wish to return to the approved version of transformers you can run the install_requirements.bat script or KoboldAI Updater.
+pause
+
+SET /P M=<loader.settings
+IF %M%==1 GOTO drivemap
+IF %M%==2 GOTO subfolder
+IF %M%==3 GOTO drivemap_B
+
+:subfolder
+ECHO Runtime launching in subfolder mode
+SET TEMP=%~DP0MINICONDA3
+SET TMP=%~DP0MINICONDA3
+call miniconda3\condabin\activate
+pip install git+https://github.com/huggingface/transformers
+cmd /k
+
+:drivemap
+ECHO Runtime launching in K: drive mode
+subst /D K: >nul
+subst K: miniconda3 >nul
+SET TEMP=K:\
+SET TMP=K:\
+call K:\python\condabin\activate
+pip install git+https://github.com/huggingface/transformers
+cmd /k
+
+:drivemap_B
+ECHO Runtime launching in B: drive mode
+subst /D B: >nul
+subst B: miniconda3 >nul
+SET TEMP=B:\
+SET TMP=B:\
+call B:\python\condabin\activate
+pip install git+https://github.com/huggingface/transformers
+cmd /k
