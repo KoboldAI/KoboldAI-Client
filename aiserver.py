a9e342ca64f8376e85d92beb9e65d246ec3997a8
somebody <onesome01@protonmail.com>
2023-05-08 17:10:47 -0500

    Fix TPU API errors

--- aiserver.py
+++ aiserver.py
@@ -3708,6 +3708,7 @@ def apiactionsubmit_tpumtjgenerate(txt, minimum, maximum):
         soft_tokens=soft_tokens,
         sampler_order=koboldai_vars.sampler_order,
     )
+    genout = np.array(genout)
     genout = [utils.applyoutputformatting(utils.decodenewlines(tokenizer.decode(txt))) for txt in genout]
 
     return genout
