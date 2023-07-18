22e7baec524caf4d113f671f923b3df905803400
Henk <henk@henk.tech>
2023-07-18 21:44:34 +0200

    Permit CPU layers on 4-bit (Worse than GGML)

--- modeling/inference_models/generic_hf_torch/class.py
+++ modeling/inference_models/generic_hf_torch/class.py
@@ -88,7 +88,8 @@ class model_backend(HFTorchInferenceModel):
                     load_in_4bit=True,
                     bnb_4bit_compute_dtype=torch.float16,
                     bnb_4bit_use_double_quant=True,
-                    bnb_4bit_quant_type='nf4'
+                    bnb_4bit_quant_type='nf4',
+                    llm_int8_enable_fp32_cpu_offload=True
                 ),
             })
 
