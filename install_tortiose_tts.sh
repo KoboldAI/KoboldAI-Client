bab52fb03503e81d07f01be7745e064b620488ba
ebolam <ebolam@gmail.com>
2023-10-14 11:32:20 -0400

    Removed tortoise tts install script

--- install_tortiose_tts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bin/micromamba run -r runtime -n koboldai pip install git+https://github.com/neonbjb/tortoise-tts OmegaConf deepspeed
-bin/micromamba run -r runtime -n koboldai pip install torchaudio --index-url https://download.pytorch.org/whl/cu118
-bin/micromamba run -r runtime -n koboldai pip install -r requirements.txt --no-dependencies
