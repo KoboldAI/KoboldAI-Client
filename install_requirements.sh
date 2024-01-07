b61dbd59fca26b03c340f4f8e5bb63bbf50f68ce
Henk <henk@henk.tech>
2024-01-07 01:11:25 +0100

    Fix WGET

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,21 +2,21 @@
 export PYTHONNOUSERSITE=1
 git submodule update --init --recursive
 if [[ $1 = "cuda" || $1 = "CUDA" ]]; then
-wget -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
+wget --no-iri -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
 bin/micromamba create -f environments/huggingface.yml -r runtime -n koboldai -y
 # Weird micromamba bug causes it to fail the first time, running it twice just to be safe, the second time is much faster
 bin/micromamba create -f environments/huggingface.yml -r runtime -n koboldai -y
 exit
 fi
 if [[ $1 = "rocm" || $1 = "ROCM" ]]; then
-wget -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
+wget --no-iri -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
 bin/micromamba create -f environments/rocm.yml -r runtime -n koboldai-rocm -y
 # Weird micromamba bug causes it to fail the first time, running it twice just to be safe, the second time is much faster
 bin/micromamba create -f environments/rocm.yml -r runtime -n koboldai-rocm -y
 exit
 fi
 if [[ $1 = "ipex" || $1 = "IPEX" ]]; then
-wget -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
+wget --no-iri -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
 bin/micromamba create -f environments/ipex.yml -r runtime -n koboldai-ipex -y
 # Weird micromamba bug causes it to fail the first time, running it twice just to be safe, the second time is much faster
 bin/micromamba create -f environments/ipex.yml -r runtime -n koboldai-ipex -y
